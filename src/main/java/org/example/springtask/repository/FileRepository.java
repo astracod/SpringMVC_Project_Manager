10db867c6ce0153a65dfdb4468fd79ab92ac3401
Zhulikov_Dmitriy <titan_22@mail.ru>
2022-02-18 23:33:15 +0300

    заменил java.io на java.nio  в FileRepository

--- src/main/java/org/example/springtask/repository/FileRepository.java
+++ src/main/java/org/example/springtask/repository/FileRepository.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.springtask.dto.Status;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -25,51 +27,29 @@ public class FileRepository {
     public static final String UNDERSCORE = "_";
     public static final String DOUBLE_BACKSLASH = "\\";
     public static final String NEL = "\n";
+    public static final String ERROR_CREATING_DIRECTORY = "Ошибка создания удаленной директории";
+    public static final String ERROR_TO_RECEIVE_DATA_FROM_REMOTE_REPOSITORY = "Ошибка получения данных с удаленного репозитория";
     public final String REMOTE_REPOSITORY = "C:\\Users\\Admin\\Desktop\\scp";
 
     public Status giveTask(LocalDateTime dateCreateTask, String text, String taskName) {
 
         String newText = dateCreateTask + NEL + text + NEL;
-        byte[] textTask = newText.getBytes();
+
         Status status = new Status();
-        File uploadDirectory = new File(REMOTE_REPOSITORY);
-        if (!uploadDirectory.exists()) {
-            uploadDirectory.mkdir();
-        }
 
         String fileName = taskName.replace(SPACE, UNDERSCORE) + FILE_EXTENSION;
-        String lastPath = uploadDirectory + DOUBLE_BACKSLASH + fileName;
-        File checkFile = new File(lastPath);
-        if (!checkFile.isFile()) {
+        String lastPath = REMOTE_REPOSITORY + DOUBLE_BACKSLASH + fileName;
+        if (!Files.isDirectory(Paths.get(REMOTE_REPOSITORY))) {
             try {
-                checkFile.createNewFile();
+                Files.createDirectories(Paths.get(REMOTE_REPOSITORY));
             } catch (IOException e) {
-                e.printStackTrace();
+                status.setStatus(ERROR_CREATING_DIRECTORY);
             }
         }
-        File resultPath = new File(lastPath);
-        FileOutputStream fos = null;
-
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream(4096)) {
-            if (!resultPath.exists()) {
-                resultPath.createNewFile();
-            }
-            out.write(textTask);
-            fos = new FileOutputStream(resultPath, true);
-            out.writeTo(fos);
-            out.flush();
-
-        } catch (Exception e) {
+        try {
+            Files.write(Paths.get(lastPath), newText.getBytes(), new StandardOpenOption[]{StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND});
+        } catch (IOException e) {
             status.setStatus(ERROR_IN_TRANSFER_OF_INFORMATION_TO_REMOTE_RESOURCE);
-            return status;
-        } finally {
-            try {
-                if (fos != null) {
-                    fos.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         Map<String, String> fileNameFromTask = new HashMap<>();
         fileNameFromTask.put("fileName", fileName);
@@ -79,16 +59,14 @@ public class FileRepository {
     }
 
     public boolean deleteFileTask(String name) {
-
         String absolutePath = REMOTE_REPOSITORY + DOUBLE_BACKSLASH + name;
         Path path = Paths.get(absolutePath);
-
         boolean res = Files.exists(path);
         if (res) {
             try {
                 Files.delete(path);
             } catch (IOException e) {
-                e.printStackTrace();
+                return false;
             }
         }
         return res;
@@ -97,15 +75,15 @@ public class FileRepository {
     public String getFileTaskByFileId(String fileName) {
         String absolutePath = REMOTE_REPOSITORY + DOUBLE_BACKSLASH + fileName;
         StringBuilder stringBuilder = new StringBuilder();
-        String line;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(absolutePath))) {
-            while ((line = reader.readLine()) != null) {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(absolutePath));
+            for (String line : lines) {
                 stringBuilder.append(line);
                 stringBuilder.append(System.lineSeparator());
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            stringBuilder.append(ERROR_TO_RECEIVE_DATA_FROM_REMOTE_REPOSITORY);
         }
         return stringBuilder.toString();
     }
